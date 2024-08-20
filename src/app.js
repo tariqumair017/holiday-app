660c0e46e0ff63016dd2b93b776374880700892c
Umair Tariq <86774981+tariqumair017@users.noreply.github.com>
2024-08-21 00:09:16 +0500

    conflict remove

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 app.use('/api', routes);
 
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
